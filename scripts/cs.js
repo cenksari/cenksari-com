52cb319ac8031cc8c425622f2c8eb309f27b02ee
Cenk SARI <cenk@cenksari.com>
2024-07-27 12:42:47 +0100

    fix theme color

--- scripts/cs.js
+++ scripts/cs.js
@@ -1,1 +1,23 @@
-const body=document.body,changeTheme=document.getElementById("mode");changeTheme.onchange=()=>{!0===changeTheme.checked?(body.classList.remove("light"),body.classList.add("dark"),window.localStorage.setItem("mode","dark")):(body.classList.remove("dark"),body.classList.add("light"),window.localStorage.setItem("mode","light"))};const mode=window.localStorage.getItem("mode");"dark"==mode&&(changeTheme.checked=!0,body.classList.remove("light"),body.classList.add("dark")),"light"==mode&&(changeTheme.checked=!1,body.classList.remove("dark"),body.classList.add("light"));
+const body = document.body,
+  changeTheme = document.getElementById("mode"),
+  appThemeColor = document.querySelector('meta[name="theme-color"]');
+changeTheme.onchange = () => {
+  !0 === changeTheme.checked
+    ? (body.classList.remove("light"),
+      body.classList.add("dark"),
+      window.localStorage.setItem("mode", "dark"),
+      appThemeColor.setAttribute("content", "#0e1117"))
+    : (body.classList.remove("dark"),
+      body.classList.add("light"),
+      window.localStorage.setItem("mode", "light"),
+      appThemeColor.setAttribute("content", "#f5f5f5"));
+};
+const mode = window.localStorage.getItem("mode");
+"dark" == mode &&
+  ((changeTheme.checked = !0),
+  body.classList.remove("light"),
+  body.classList.add("dark")),
+  "light" == mode &&
+    ((changeTheme.checked = !1),
+    body.classList.remove("dark"),
+    body.classList.add("light"));
